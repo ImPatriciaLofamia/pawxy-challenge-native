04f0b4b3e4868f6808e3678d8af6ee3f789afca6
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-05-30 14:55:44 +0800

    configuration for dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
-    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+    "./lib/**/*.{js,jsx,ts,tsx}",
+    "./App/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js, jsx, ts, tsx}",
+
   ],
   theme: {
     extend: {},
